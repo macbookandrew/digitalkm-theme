4fb30a2e19a187bb1267013284507c36d509e6ff
Andrew Minion <andrew@andrewrminion.com>
2018-03-12 16:00:04 -0400

    add SASS & build tooling

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+	// Watch task config
+	watch: {
+		styles: {
+			files: "src/sass/**/*.scss",
+			tasks: ['sass', 'postcss'],
+		},
+		javascript: {
+			files: ["src/js/*.js"],
+			tasks: ['uglify'],
+		},
+		svg: {
+			files: ["src/img/*.svg"],
+			tasks: ['svgmin'],
+		},
+	},
+	sass: {
+		dist: {
+			options: {
+				style: 'compressed'
+			},
+			files: {
+				"assets/css/style.min.css" : "src/sass/**/style.scss",
+			}
+		}
+	},
+	postcss: {
+		options: {
+			map: {
+				inline: false,
+				annotation: 'assets/css/',
+			},
+
+			processors: [
+				require('pixrem')(), // add fallbacks for rem units
+				require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+				require('cssnano')() // minify the result
+			]
+		},
+		dist: {
+			src: 'assets/css/*.css',
+		}
+	},
+	svgmin: {
+		options: {
+			plugins: [
+				{
+					removeViewBox: false
+				},
+				{
+					removeUselessStrokeAndFill: false
+				}
+			]
+		},
+		dist: {
+			files: [
+				{
+					expand: true,
+					cwd: 'src/img',
+					src: '*.svg',
+					dest: 'assets/img/'
+				}
+			]
+		}
+	},
+	uglify: {
+		custom: {
+			files: {
+				'assets/js/customizer.min.js': ['src/js/customizer.js'],
+				'assets/js/navigation.min.js': ['src/js/navigation.js'],
+				'assets/js/skip-link-focus-fix.min.js': ['src/js/skip-link-focus-fix.js']
+			},
+		},
+	},
+	browserSync: {
+		dev: {
+			bsFiles: {
+				src : ['assets/css/*.css', '**/*.php', 'assets/js/*.js', '**/*.svg', '!node_modules'],
+			},
+			options: {
+				watchTask: true,
+				proxy: "https://digitalkm.wordpress.dev",
+				open: "external",
+				host: "andrews-macbook-pro.local",
+				https: {
+					key: "/Users/andrew/github/dotfiles/local-dev.key",
+					cert: "/Users/andrew/github/dotfiles/local-dev.crt",
+				}
+			},
+		},
+	},
+  });
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-svgmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.registerTask('default', [
+		'browserSync',
+		'watch',
+	]);
+};
--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@
  * @package DigitalKM
  */
 
+define( 'DKM_THEME_VERSION', wp_get_theme()->get( 'Version' ) );
+
 if ( ! function_exists( 'digitalkm_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
@@ -120,11 +122,11 @@ add_action( 'widgets_init', 'digitalkm_widgets_init' );
  * Enqueue scripts and styles.
  */
 function digitalkm_scripts() {
-	wp_enqueue_style( 'digitalkm-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'digitalkm-style', get_template_directory_uri() . '/assets/css/style.min.css', array(), DKM_THEME_VERSION );
 
-	wp_enqueue_script( 'digitalkm-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'digitalkm-navigation', get_template_directory_uri() . '/assets/js/navigation.min.js', array(), DKM_THEME_VERSION, true );
 
-	wp_enqueue_script( 'digitalkm-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'digitalkm-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.min.js', array(), DKM_THEME_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
