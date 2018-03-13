a930bd2289503a2d95c34ad7c18fc5a015c42a7b
Andrew Minion <andrew@andrewrminion.com>
2018-03-13 14:22:54 -0400

    add KMHM and MML to footer

--- footer.php
+++ footer.php
@@ -15,17 +15,12 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'digitalkm' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'digitalkm' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'digitalkm' ), 'DigitalKM', '<a href="https://andrewrminion.com/">AndrewRMinion Design</a>' );
-				?>
+			<p>A joint project between the <a href="http://www.kingsmountainmuseum.org/">Kings Mountain Historical Museum</a> and <a href="http://mauneylibrary.org/">Mauney Memorial Library</a>.</p>
+			<p>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'digitalkm' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'digitalkm' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'digitalkm' ), 'DigitalKM', '<a href="https://andrewrminion.com/">AndrewRMinion Design</a>' ); ?>
+			</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
