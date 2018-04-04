d7834c3baac7212f6f4cc0e9f48daa0d1b93e00c
Andrew Minion <andrew@andrewrminion.com>
2018-04-04 13:05:33 -0400

    tweak footer credits

--- footer.php
+++ footer.php
@@ -15,11 +15,11 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<p>A joint project between the <a href="http://www.kingsmountainmuseum.org/">Kings Mountain Historical Museum</a> and <a href="http://mauneylibrary.org/">Mauney Memorial Library</a>.</p>
+			<p>A joint project between the <a href="http://www.kingsmountainmuseum.org/">Kings Mountain Historical Museum</a> and <a href="http://mauneylibrary.org/">Mauney Memorial Library</a></p>
 			<p>
 				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'digitalkm' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'digitalkm' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
-				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'digitalkm' ), 'DigitalKM', '<a href="https://andrewrminion.com/">AndrewRMinion Design</a>' ); ?>
+				Custom development by <a href="https://andrewrminion.com/">AndrewRMinion Design</a>
 			</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
